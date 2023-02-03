194c7bf65960e6362d5dc544c045db7fa6ed4ebb
xiaolong-snnu <65990576+xiaolong-snnu@users.noreply.github.com>
2023-02-03 09:04:58 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -2,30 +2,35 @@ from train import test
 from data import sample_data
 
 if __name__ == '__main__':
-    # Step 1: select a config file
+    # Step 1: select one config file and the corresponding model file
+    ################################
+    # The OU system
+    ################################
     from config_ou import Config, sampling_func  # train the OU process
-    # from config_gene_switch import Config, sampling_func  # train the genetic toggle switch system
-    # from config_duffing import Config, sampling_func  # train the Duffing system
     config = Config()
-
-    # Step 2: Sample the data.
-    # Comment this line if the data has been generated.
-    sample_data(config, sampling_func, mode='test')
-
-    # Step 3: Select the model file
     config.param['test_model_file'] = './data/ou/model_00988.ckpt'
 
-    # Step 4: Test
-    test(config)
-
-
-
-    # Another example
-    # from config_gene_switch import Config, sampling_func
+    ################################
+    # The genetic toggle switch system
+    ################################
+    # from config_gene_switch import Config, sampling_func  # train the genetic toggle switch system
     # config = Config()
-    # config.param['test_model_file'] = './data/gene_switch/debug/model/model_00003.ckpt'
-    # config.param['test_file'] = config.param['train_file']
-    # test(config)
+    # config.param['test_model_file'] = './data/gene_switch/model_00986.ckpt'
+
+    ################################
+    # The duffing system
+    ################################
+    # from config_duffing import Config, sampling_func  # train the Duffing system
+    # config = Config()
+    # config.param['test_model_file'] = './data/duffing/model_00475.ckpt'
+    ###################################################
+
+    # Step 2: Sample the test data.
+    # Comment out this line if the data has been generated.
+    sample_data(config, sampling_func, mode='test')
+
+    # Step 3: Test
+    test(config)
 
 
 
